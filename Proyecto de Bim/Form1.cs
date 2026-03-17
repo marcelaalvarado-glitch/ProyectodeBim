e500125414e6ae0d1366916e7d1ec1e35bf2409c
4º PC <4 PC@DESKTOP-6342417>
2026-03-17 18:30:25 +0100

    suma

--- Proyecto de Bim/Form1.cs
+++ Proyecto de Bim/Form1.cs
@@ -23,14 +23,45 @@ namespace Proyecto_de_Bim
             {
                  MessageBox.Show("ERROR, ingrese un numero en el primer campo.");
             }
-        }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
             if (textBox1.Text == "")
             {
                 MessageBox.Show("ERROR, ingrese un numero en el primer campo.");
             }
+
+            //checar si lo ingresado es un numero
+
+            double num1, num2;
+
+            if (!double.TryParse(textBox1.Text, out num1))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el primer campo.");
+                return;
+            }
+
+            if (!double.TryParse(textBox2.Text, out num2))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el segundo campo.");
+                return;
+            }
+
+            double resultado = num1 + num2;
+
+            label3.Text = resultado.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
