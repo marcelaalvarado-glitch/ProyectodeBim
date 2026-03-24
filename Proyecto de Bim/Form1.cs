5b474e218a86e330cc66b90bb2877834f31d519c
4º PC <4 PC@DESKTOP-6342417>
2026-03-24 18:20:37 +0100

    Calculadora Completa

--- Proyecto de Bim/Form1.cs
+++ Proyecto de Bim/Form1.cs
@@ -63,5 +63,116 @@ namespace Proyecto_de_Bim
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //checar si lo ingresado es un numero
+
+            double num1, num2;
+
+            if (!double.TryParse(textBox1.Text, out num1))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el primer campo.");
+                return;
+            }
+
+            if (!double.TryParse(textBox2.Text, out num2))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el segundo campo.");
+                return;
+            }
+
+            double resultado2 = num1 - num2;
+
+            label4.Text = resultado2.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            double num1, num2;
+
+            if (!double.TryParse(textBox1.Text, out num1))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el primer campo.");
+                return;
+            }
+
+            if (!double.TryParse(textBox2.Text, out num2))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el segundo campo.");
+                return;
+            }
+
+            double resultado3 = num1 * num2;
+
+            label5.Text = resultado3.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if (!double.TryParse(textBox1.Text, out num1))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el primer campo.");
+                return;
+            }
+
+            if (!double.TryParse(textBox2.Text, out num2))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el segundo campo.");
+                return;
+            }
+
+            double resultado4 = num1 / num2;
+
+            label6.Text = resultado4.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if (!double.TryParse(textBox1.Text, out num1))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el primer campo.");
+                return;
+            }
+
+            if (!double.TryParse(textBox2.Text, out num2))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el segundo campo.");
+                return;
+            }
+
+            double resultado5 = Math.Pow(num1, num2);
+
+            label7.Text = resultado5.ToString();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+            if (!double.TryParse(textBox1.Text, out num1))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el primer campo.");
+                return;
+            }
+            if (!double.TryParse(textBox2.Text, out num2))
+            {
+                MessageBox.Show("ERROR, ingrese un numero valido en el segundo campo.");
+                return;
+            }
+            double resultado6 = Math.Sqrt(num1);
+            label8.Text = resultado6.ToString();
+        }
     }
 }
